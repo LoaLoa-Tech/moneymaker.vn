b3d95615740a6ab7741e7d8732d6703c35197cf1
Robert Tran <truongduchuy910@gmail.com>
2021-01-04 03:06:25 +0700

    build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 /* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 import "./App.css";
 import Content from "./components/Content";
+import { TradingViewEmbed, widgetType } from "react-tradingview-embed";
+
 function App() {
+  const trading = ` <iframe
+          scrolling="no"
+          allowtransparency="true"
+          style="box-sizing: border-box; height: 46px; width: 100%"
+          src="https://s.tradingview.com/embed-widget/ticker-tape/?locale=en#%7B%22symbols%22%3A%5B%7B%22proName%22%3A%22FX_IDC%3AEURUSD%22%2C%22title%22%3A%22EUR%2FUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AAUDUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AGBPUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22CURRENCYCOM%3AGOLD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AUSDJPY%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22USDCHF%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22USDCAD%22%7D%5D%2C%22colorTheme%22%3A%22light%22%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A46%2C%22utm_source%22%3A%22moneymaker.vn%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_campaign%22%3A%22ticker-tape%22%7D"
+          frameborder="0"
+        ></iframe>`;
   return (
     <div className="App">
       <header className="mb-5">
-        {/* <iframe
-        scrolling="no"
-        allowtransparency="true"
-        style="box-sizing: border-box; height: 46px; width: 100%"
-        src="https://s.tradingview.com/embed-widget/ticker-tape/?locale=en#%7B%22symbols%22%3A%5B%7B%22proName%22%3A%22FX_IDC%3AEURUSD%22%2C%22title%22%3A%22EUR%2FUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AAUDUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AGBPUSD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22CURRENCYCOM%3AGOLD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22OANDA%3AUSDJPY%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22USDCHF%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22USDCAD%22%7D%5D%2C%22colorTheme%22%3A%22light%22%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A46%2C%22utm_source%22%3A%22moneymaker.vn%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_campaign%22%3A%22ticker-tape%22%7D"
-        frameborder="0"
-      ></iframe>  */}
+        <TradingViewEmbed
+          widgetType={widgetType.TICKER_TAPE}
+          widgetConfig={{
+            colorTheme: "dark",
+            symbol: "BITMEX:XBTUSD",
+            width: "100%",
+          }}
+        />
         <div className="container">
           <div className="row">
             <div className="col-sm-12 col-md-6 col-lg-5">
@@ -56,7 +67,16 @@ function App() {
           </div>
         </div>
       </section>
-
+      <section className="container">
+        <TradingViewEmbed
+          widgetType={widgetType.ADVANCED_CHART}
+          widgetConfig={{
+            colorTheme: "dark",
+            symbol: "BITMEX:XBTUSD",
+            width: "100%",
+          }}
+        />
+      </section>
       <div className="footer-dark">
         <footer>
           <div className="container">
