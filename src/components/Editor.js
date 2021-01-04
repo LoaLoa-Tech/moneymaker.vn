41bc8af26c7fd15ef5da4262f9b337c8c399c14a
Robert Tran <truongduchuy910@gmail.com>
2021-01-04 22:30:13 +0700

    build

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -46,6 +46,15 @@ const Editor = ({ type }) => {
         init={{
           height: 500,
           menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | image code link | help",
         }}
         value={data.allPosts[0]?.body}
         onEditorChange={handleEditorChange}
